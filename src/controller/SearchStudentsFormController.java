f2d83c9a884927b3428fb2312e76c1970692dac1
Prasad Viduranga <pkpviduranga@gmail.com>
2021-08-23 11:17:16 +0530

    Add action panel to the table

--- src/controller/SearchStudentsFormController.java
+++ src/controller/SearchStudentsFormController.java
@@ -1,8 +1,14 @@
 package controller;
 
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.util.Callback;
 import model.Student;
 import model.StudentTM;
 import service.StudentService;
@@ -22,6 +28,21 @@ public class SearchStudentsFormController {
         tblResults.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("fullName"));
         tblResults.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("address"));
 
+        TableColumn<StudentTM, HBox> lastCol = (TableColumn<StudentTM, HBox>) tblResults.getColumns().get(3);
+
+        lastCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<StudentTM, HBox>, ObservableValue<HBox>>() {
+            @Override
+            public ObservableValue<HBox> call(TableColumn.CellDataFeatures<StudentTM, HBox> param) {
+                ImageView imgEdit = new ImageView("/view/assets/Edit.png");
+                ImageView imgTrash = new ImageView("/view/assets/Trash.png");
+
+                imgEdit.getStyleClass().add("action-icons");
+                imgTrash.getStyleClass().add("action-icons");
+
+                return new ReadOnlyObjectWrapper<>(new HBox(10, imgEdit, imgTrash));
+            }
+        });
+
         txtQuery.textProperty().addListener((observable, oldValue, newValue) -> loadAllStudents(newValue));
 
         loadAllStudents(null);
@@ -30,7 +51,7 @@ public class SearchStudentsFormController {
     private void loadAllStudents(String query){
         tblResults.getItems().clear();
 
-        for (Student student : studentService.findStudents(query == null || query.trim().isEmpty() ? "": query)) {
+        for (Student student : studentService.findStudents(query)) {
             tblResults.getItems().add(new StudentTM(student.getNic(), student.getFullName(), student.getAddress()));
         }
 
